c2adeaeb40419b0bb64d54af60ea571ba34073cc
iqbalmdd <iqbalmuhammad1290@gmail.com>
2024-07-20 17:32:59 +0700

    kurang logo

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import { Link } from "expo-router";
+import { Text, ScrollView, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+
 // import { useGlobalContext } from "../context/GlobalProvider";
 
 const Welcome = () => {
@@ -11,7 +12,12 @@ const Welcome = () => {
 
   return (
     <SafeAreaView className="bg-secondary h-full flex justify-center">
-      <View className="mt-40">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+      <View className="flex w-full h-full justify-end pb-40">
         <View className="flex justify-center h-auto items-start pl-8">
           <Text className="text-white font-bold text-4xl">
             Your
@@ -22,11 +28,15 @@ const Welcome = () => {
           </Text>
         </View>
         <View className="flex items-center w-full h-10 my-5">
-          <TouchableOpacity className="bg-primary w-5/6 rounded-full h-full flex items-center justify-center">
+          <TouchableOpacity 
+          className="bg-primary w-5/6 rounded-full h-full flex items-center justify-center"
+          onPress={() => router.push("/sign-in")}
+          >
             <Text className="text-secondary-200 font-bold">Sign In</Text>
           </TouchableOpacity>
         </View>
       </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }

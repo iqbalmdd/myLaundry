7b995db08c9fb35b786b08d95eac00317a9d6ca2
iqbalmdd <iqbalmuhammad1290@gmail.com>
2024-07-20 17:19:27 +0700

    [welcome]

--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { Link } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+// import { useGlobalContext } from "../context/GlobalProvider";
 
-export default function App() {
+const Welcome = () => {
+  // const { loading, isLogged } = useGlobalContext();
+
+  // if (!loading && isLogged) return <Redirect href="/home" />;
+
   return (
     <SafeAreaView className="bg-secondary h-full flex justify-center">
-      <View className="flex justify-center h-auto items-start pl-8">
-      <Text className="text-white font-bold text-4xl">The Only</Text>
-      <Text className="text-primary font-extrabold text-5xl">Laundry</Text>
-      <Text className="text-white font-bold text-4xl">App You Need!</Text>
+      <View className="mt-40">
+        <View className="flex justify-center h-auto items-start pl-8">
+          <Text className="text-white font-bold text-4xl">
+            Your
+            <Text className="text-primary font-extrabold"> Laundry</Text>
+          </Text>
+          <Text className="text-white font-extrabold text-5xl">
+            Simplified.
+          </Text>
+        </View>
+        <View className="flex items-center w-full h-10 my-5">
+          <TouchableOpacity className="bg-primary w-5/6 rounded-full h-full flex items-center justify-center">
+            <Text className="text-secondary-200 font-bold">Sign In</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View className="flex items-center w-full h-10 my-5">
-      <TouchableOpacity className="bg-primary w-5/6 rounded-full h-full flex items-center justify-center" >
-        <Text>Sign In</Text>
-      </TouchableOpacity>
-
-      </View>
-
     </SafeAreaView>
   );
 }
 
+export default Welcome;

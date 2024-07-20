01716ba932f0f0e13aca349926e7f95d4e9bc7d7
iqbalmdd <iqbalmuhammad1290@gmail.com>
2024-07-20 19:47:13 +0700

    [Sign-In] Scroll view still disabled

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,41 +1,56 @@
-import { ScrollView, StyleSheet, Text, View, Dimensions, TextInput } from 'react-native'
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { useState } from "react";
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from "react-native-safe-area-context";
 import { FormField } from "../../components";
 
 const signIn = () => {
   const [form, setForm] = useState({
-    email:"",
-    password:"",
+    email: "",
+    password: "",
   });
   return (
-    <SafeAreaView className="bg-secondary-100 h-full flex justify-center">
-      <ScrollView>
-          <View className="h-full w-full px-4 flex items-center">
-            <Text className="text-primary text-3xl font-pextrabold">Smart Laundry</Text>
-            <View className="flex h-full w-full items-start px-5 mt-7 bg-secondary rounded-lg">
-              <Text className="font-bold font-2xl mt-3">Sign In</Text>
-              <FormField
-            placeholder="Email"
-            value={form.email}
-            handleChangeText={(e) => setForm({ ...form, email: e })}
-            otherStyles="mt-2"
-            keyboardType="email-address"
-          />
-          <FormField
-            placeholder="Password"
-            value={form.password}
-            handleChangeText={(e) => setForm({ ...form, password: e })}
-            otherStyles="mt-2"
-          />
+    <SafeAreaView className="bg-secondary-100 h-full">
+      {/* <ScrollView> */}
+        <View className="h-full w-full flex items-start ">
+          <View>
+          <Text className="text-white text-3xl font-pextrabold ml-5 mt-7">
+            Welcome!
+          </Text>
+          <Text className="text-white text-3xl font-pextrabold ml-5">
+            Smart Laundry
+          </Text>
+          </View>
+          <View className="flex h-full w-full items-start px-5 mt-20 bg-white rounded-3xl">
+            <Text className="font-bold font-2xl mt-3">Sign In</Text>
+            <FormField
+              placeholder="Email"
+              value={form.email}
+              handleChangeText={(e) => setForm({ ...form, email: e })}
+              keyboardType="email-address"
+            />
+            <FormField
+              placeholder="Password"
+              value={form.password}
+              handleChangeText={(e) => setForm({ ...form, password: e })}
+            />
+            <View className="flex items-center w-full h-12 my-5">
+              <TouchableOpacity className="bg-secondary-200 w-full rounded-xl h-full flex items-center justify-center">
+                <Text className="text-white font-bold">Sign In</Text>
+              </TouchableOpacity>
             </View>
           </View>
-
-      </ScrollView>
+        </View>
+      {/* </ScrollView> */}
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default signIn
-
-const styles = StyleSheet.create({})
+export default signIn;
--- components/FormField.jsx
+++ components/FormField.jsx
@@ -14,10 +14,10 @@ const FormField = ({
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <View className={`space-y-2 ${otherStyles}`}>
+    <View className={`space-y-1 ${otherStyles}`}>
       <Text className="text-base text-secondary-300 font-pmedium">{title}</Text>
 
-      <View className="w-full h-12 px-4 bg-secondary rounded-xl border-2 border-secondary-200 flex flex-row items-center">
+      <View className="w-full h-12 px-4 bg-white rounded-xl border-2 border-secondary-200 flex flex-row items-center">
         <TextInput
           className="flex-1 text-secondary-300 font-psemibold text-base"
           value={value}

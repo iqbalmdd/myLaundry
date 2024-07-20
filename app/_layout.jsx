648a4af9859ec36e4cdd5c67dbd67451fcabae54
iqbalmdd <iqbalmuhammad1290@gmail.com>
2024-07-20 13:44:57 +0700

    initial structure

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Slot, Stack } from 'expo-router'
 
 const RootLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    <Stack>
+        <Stack.Screen name='index' options={{headerShown:false}} />
+    </Stack>
   )
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({})
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>My Laundry</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
